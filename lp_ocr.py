c3dcc7d4c8290263106b85920007c88dae1aa4f8
pooja-aryamane <70020869+pooja-aryamane@users.noreply.github.com>
2022-02-01 17:56:56 +0530

    Made layout inference and lp ocr more independent

--- lp_ocr.py
+++ lp_ocr.py
@@ -18,22 +18,11 @@ import subprocess
 # Execute layout inference
 
 from layout_inference import infer_layout 
-# taking the folder that has layout detections
-output_folderName = infer_layout()
 
-#initialise input image
-'''
-fnames = [fn for fn in os.listdir('.')]
-fnames_= {}
-for k,f in enumerate(fnames):
-  print(str(k+1) + ". " + str(f))
-  fnames_[str(k+1)] = str(f)
-dir_no = int(input("Please enter the image directory number:\n"))
-img_dir = fnames_[str(dir_no)]
-print(dir_no, "-", img_dir)
-'''
+#does the user want to use layout inference ? 
+infer_flag = input("Do you wish to use Layout Inference? (yes or no)")
 
-if(input('Do you want to specify Tessdata directory? \nDefault: $(LOCAL)/share/tessdata \nEnter y for yes, n for no: ') == 'y'):
+if(input('Do you wish to specify Tessdata directory? \nDefault: $(LOCAL)/share/tessdata \nEnter y for yes, n for no: ') == 'y'):
   os.environ["TESSDATA_PREFIX"] = input("Enter path to tessdata directory: \n")
 
 #initialise language model 
@@ -41,59 +30,64 @@ input_lang = input("Please enter the language of your images. If more than one l
 
 #initialise output directory 
 try:
-  output_dir = input("Please enter the name of the output directory:\n")
+  output_dir = input("Directory for OCR output: \n")
   if(output_dir.find(" ")!=-1):
     raise NameError("File name contains spaces")
 except Exception as err:
   print("Error: {0}".format(err))
   sys.exit(1)
 
-  #if user inputs an existing directory, no new directory will be formed. Instead only the output_dir variable will be changed
 if not os.path.exists(output_dir):
   os.mkdir(output_dir) 
-  
+
 ocr_agent = lp.TesseractAgent(languages=input_lang) 
 
-for layout_class in os.listdir(output_folderName):
- # only take images from class-wise folders with cropped images and not the image with visualisation of predictions 
- if not layout_class.endswith('.jpg') or layout_class.endswith('.png') or layout_class.endswith('.jpeg'):
-   img_dir = output_folderName + '/' +layout_class
- else:
-   continue 
- if os.path.isdir(img_dir):
-   for img_file in os.listdir(img_dir):
-     if img_file.endswith('.pdf'):
-       print("OCR-ing pdfs...\n")
-       newdir = output_dir + "/" + img_file.replace(".pdf", "")
-       os.mkdir(newdir)
-       os.mkdir(newdir + "/page_images")
-       os.mkdir(newdir + "/output")
-       img_path= img_dir + "/" + img_file
-       print("Converting to images...\n")
-       convert_from_path(img_path,
-           output_folder= newdir + "/page_images",
-           paths_only=True,
-           fmt='jpg',
-           output_file="O",
-           use_pdftocairo=True,
-         )
-       for img_ in os.listdir(newdir + "/page_images"):
-         print(img_)
-         image = cv2.imread(newdir + "/page_images/" + img_)
-         res = ocr_agent.detect(image)
-         with open(newdir + "/output/" + img_[:-4] + 'txt', 'w') as f:
-           f.write(res)
-     elif img_file.endswith('.jpg') or img_file.endswith('.png') or img_file.endswith('.jpeg'):
-       print("OCR-ing images...\n")
-       image = cv2.imread(img_dir + "/" + img_file)
-       res = ocr_agent.detect(image)
-       if img_file.endswith('.jpeg'):
-         x = img_file[:-5]
-       else:
-         x = img_file[:-4]
-       with open(output_dir + '/' + x + '.txt', 'w') as f:
-         f.write(res)
+if infer_flag == "no":
+  img_dir = input("Enter the name of our image folder for OCR")
+  if os.path.isdir(img_dir):
+    for img_file in os.listdir(img_dir):
+      if img_file.endswith('.pdf'):
+        print("OCR-ing pdfs...\n")
+        newdir = output_dir + "/" + img_file.replace(".pdf", "")
+        os.mkdir(newdir)
+        os.mkdir(newdir + "/page_images")
+        os.mkdir(newdir + "/output")
+        img_path= img_dir + "/" + img_file
+        print("Converting to images...\n")
+        convert_from_path(img_path,
+            output_folder= newdir + "/page_images",
+            paths_only=True,
+            fmt='jpg',
+            output_file="O",
+            use_pdftocairo=True,
+          )
+        for img_ in os.listdir(newdir + "/page_images"):
+          print(img_)
+          image = cv2.imread(newdir + "/page_images/" + img_)
+          res = ocr_agent.detect(image)
+          with open(newdir + "/output/" + img_[:-4] + 'txt', 'w') as f:
+            f.write(res)
+      elif img_file.endswith('.jpg') or img_file.endswith('.png') or img_file.endswith('.jpeg'):
+        print("OCR-ing images...\n")
+        image = cv2.imread(img_dir + "/" + img_file)
+        res = ocr_agent.detect(image)
+        if img_file.endswith('.jpeg'):
+          x = img_file[:-5]
+        else:
+          x = img_file[:-4]
+        with open(output_dir + '/' + x + '.txt', 'w') as f:
+          f.write(res)
+  print("OCR is complete. Please find the output in the provided output directory.")
 
-   print("OCR is complete. Please find the output in the provided output directory.")
- else:
-   continue
+elif infer_flag == "yes":
+  img, layout_info = infer_layout()
+  for label, box in layout_info.items():
+    img_cropped = img.crop(box)
+    res = ocr_agent.detect(img_cropped)
+    with open(output_dir + '/' + label + '.txt', 'w') as f:
+      f.write(res)
+  
+  print("OCR is complete. Please find the output in the provided output directory.")
+
+else:
+  print('Incorrect Input')
